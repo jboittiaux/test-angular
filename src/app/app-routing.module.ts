ede5504ed118b3ce750a08d6c31b85001af205a3
Jean BOITTIAUX <jean.boittiaux@gmail.com>
2022-05-01 22:48:50 +0200

    Mise en place des layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FooterOnlyLayoutComponent } from "./layout/footer-only-layout/footer-only-layout.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginModule} from './login/login.module';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {RegistrationModule} from './registration/registration.module';
+import {LayoutModule} from './layout/layout.module';
+import {UsersModule} from './users/users.module';
+import {AccountSettingsModule} from './account-settings/account-settings.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    LayoutModule,
+    LoginModule,
+    DashboardModule,
+    RegistrationModule,
+    UsersModule,
+    AccountSettingsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
